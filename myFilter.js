84df53997e64a667231ff7797c3e74b1eecdbb9b
DeepWebD <deepak.thewebdevloper@gmail.com>
2025-12-30 20:35:42 +0530

    filter polyfill

---
+++ myFilter.js
@@ -0,0 +1,15 @@
+if(Array.prototype.myFilter===undefined){
+    Array.prototype.myFilter= function(callback,thisArg){
+       if(this===null) throw new throwError("Array.filter.myFilter called on null or undefined");
+       if(typeof callback !== 'function') throw new throwError('Callback must be a function');
+
+       const result=[];
+       for(let i=0;i<this.length;i++){
+        if(callback.call(thisArg,this[i],i,this)) result.push(this[i])
+       }
+
+       return result;
+    }
+}
+
+console.log([2,3, ,4,5].filter((num)=>num%2==0))
